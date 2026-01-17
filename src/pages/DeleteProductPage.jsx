bcb606a5bba91e68d23edb3ba63302b181816685
leandroArias1 <Wazzybtw@gmail.com>
2026-01-16 21:29:27 -0300

    commit2

--- src/pages/DeleteProductPage.jsx
+++ src/pages/DeleteProductPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { createProduct } from '../api/api';
+import { deleteProduct } from '../api/api';
 
 export default function DeleteProductPage() {
   const [form, setForm] = useState({
